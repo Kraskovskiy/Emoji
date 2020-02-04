8171e5c39df9040e07b290dc7369b08158d1c6c4
Kraskovskiy <KraskovskiyAB@gmail.com>
2020-02-04 11:22:02 +0300

    fix size v2

--- emoji/src/main/java/com/vanniktech/emoji/category/StickersPackThreeCategory.java
+++ emoji/src/main/java/com/vanniktech/emoji/category/StickersPackThreeCategory.java
@@ -10,18 +10,7 @@ import com.vanniktech.emoji.emoji.EmojiCategory;
 
 @SuppressWarnings("PMD.MethodReturnsInternalArray") public final class StickersPackThreeCategory implements EmojiCategory {
     private static final Emoji[] DATA = new Emoji[] {
-            new Emoji(300001, R.drawable.stickers_pack_3_n1, true),
-            new Emoji(300002, R.drawable.stickers_pack_3_n2, true),
-            new Emoji(300003, R.drawable.stickers_pack_3_n3, true),
-            new Emoji(300004, R.drawable.stickers_pack_3_n4, true),
-            new Emoji(300005, R.drawable.stickers_pack_3_n5, true),
-            new Emoji(300006, R.drawable.stickers_pack_3_n6, true),
-            new Emoji(300007, R.drawable.stickers_pack_3_n7, true),
-            new Emoji(300008, R.drawable.stickers_pack_3_n8, true),
-            new Emoji(300009, R.drawable.stickers_pack_3_n9, true),
-            new Emoji(300010, R.drawable.stickers_pack_3_n10, true),
-            new Emoji(300011, R.drawable.stickers_pack_3_n11, true),
-            new Emoji(300012, R.drawable.stickers_pack_3_n12, true)
+
     };
 
     @Override @NonNull
@@ -31,6 +20,6 @@ import com.vanniktech.emoji.emoji.EmojiCategory;
 
     @Override @DrawableRes
     public int getIcon() {
-        return R.drawable.stickers_pack_3_n2;
+        return R.drawable.sticker_emoji;
     }
 }
